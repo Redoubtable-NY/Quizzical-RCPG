5f210d56da874e39f88b43df9c8678652b9b3360
Stephon Odom <stephon.odom7@gmail.com>
2025-08-02 18:01:58 -0400

    Introduce new state variables for quiz answer choices and quiz questions. Utilize new he library to decode HTML entities within questions that are provided by the API. Pass down quiz answer choices and quiz questions to Question view component instead of establishing them there. Add fisher yates algorithim to use effect to ensure the randomization of all answer choices is less prone to bias that could make the selection of the correct answer easier over time.

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,40 @@ import { use, useState } from 'react'
 import { useEffect } from 'react'
 import StartView from './StartView.jsx'
 import QuestionsView from "./QuestionView.jsx"
+import he from "he"
 
 function App() {
   const [quizStart, setQuizStart] = useState(true)
-  const [quizContents, setQuizContents] = useState('')
   const [newRound, setNewRound] = useState(false)
+  const [quizQuestions, setQuizQuestions] = useState([])
   const [isAnswersChecked, setIsAnswersChecked] = useState(false)
   const [selectedAnswer, setSelectedAnswer] = useState('')
+  const [quizRandomizedAnswers, setQuizRandomizedAnswers] = useState([])
 
   useEffect(() => {
     fetch("https://opentdb.com/api.php?amount=5")
       .then(resp => resp.json())
-      .then(data => setQuizContents(data))
+      .then((data) => {
+        console.log(data.results)
+        const quizQuestionsAndAnswers = data.results
+        const quizQuestionResponses = quizQuestionsAndAnswers.map((arrayItem) => he.decode(arrayItem.question))
+        console.log(quizQuestionResponses)
+        const quizAnswerKey = quizQuestionsAndAnswers.map(arrayItem => arrayItem.correct_answer)
+        const quizAnswerChoices = quizQuestionsAndAnswers.map((arrayItem, index) => {
+                return arrayItem.incorrect_answers.concat(quizAnswerKey[index])
+            }
+        )
+        setQuizQuestions(quizQuestionResponses)
+        console.log(quizAnswerChoices)
+        for(const answerChoice of quizAnswerChoices){
+            for(let i = answerChoice.length - 1; i >= 1; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [answerChoice[i], answerChoice[j]] = [answerChoice[j], answerChoice[i]];
+            }
+        }
+        setQuizRandomizedAnswers(quizAnswerChoices)
+      }
+    )
   },[ ])
 
   function startGame(){
@@ -37,11 +59,13 @@ function App() {
      {quizStart ? 
       (<StartView handleClick={startGame}/>) : 
       (<QuestionsView 
-            quizContents={quizContents} 
             isAnswersChecked={isAnswersChecked} 
             handleCheckClick={answerChecker}
             selectedAnswer={selectedAnswer}
             handleAnswerChoiceClick={selectAnswerChoice}
+            setQuizRandomizedAnswers={setQuizRandomizedAnswers}
+            quizRandomizedAnswers={quizRandomizedAnswers}
+            quizQuestions={quizQuestions}
         />
         ) 
       }
--- src/QuestionView.jsx
+++ src/QuestionView.jsx
@@ -4,26 +4,8 @@ import { nanoid } from 'nanoid'
 import { clsx } from 'clsx'
 
 export default function QuestionsView(props){
-    const quizQuestionsAndAnswers = props.quizContents.results
-    const quizQuestions = quizQuestionsAndAnswers.map((arrayItem) => arrayItem.question)
-    const quizAnswerKey = []
-    const quizAnswerChoices = [] 
-    const quizAnswers = quizQuestionsAndAnswers.map((arrayItem) => {
-        quizAnswerChoices.push(arrayItem.incorrect_answers)
-        quizAnswerKey.push(arrayItem.correct_answer)
-    })
-
-    console.log(quizQuestionsAndAnswers)
-    console.log(quizQuestions)
-    console.log(quizAnswerChoices)
-    console.log(quizAnswerKey)
     
-    for(let i = 0 ; i < quizAnswerChoices.length; i++){
-        const randomValue = Math.floor(quizAnswerKey[i].length * Math.random())
-        quizAnswerChoices[i].splice(randomValue, 0, quizAnswerKey[i])
-    }
-    
-    const questionOneAnswerChoices = quizAnswerChoices[0].map((questionCollection, index) => 
+    const questionOneAnswerChoices = props.quizRandomizedAnswers[0].map((questionCollection, index) => 
         {   
             // const isCorrect = quizAnswerKey.includes(questionCollection)
             // className={clsx(
@@ -31,7 +13,14 @@ export default function QuestionsView(props){
             // )}
             return(
                 <div key={nanoid()} className='answer-choice-container'>
-                    <input key={nanoid()} type="radio" id={`button: ${index}`} name="question-one" value={questionCollection} checked={props.selectedAnswer} onChange={props.handleAnswerChoiceClick}/>
+                    <input key={nanoid()} 
+                        type="radio" 
+                        name="question-one" 
+                        id={`button: ${index}`} 
+                        value={questionCollection} 
+                        checked={props.selectedAnswer === `${questionCollection}`} 
+                        onChange={props.handleAnswerChoiceClick}
+                    />
                     <label key={nanoid()} htmlFor={`button: ${index}`}
                         >{questionCollection}
                     </label>
@@ -39,23 +28,21 @@ export default function QuestionsView(props){
             )
         }
     )
-    
 
-    console.log(quizAnswerChoices)
     return(
         <>
             <img id="Quest-yellow-blob" src={QuestYellowBlob} alt='decorative image'/>
             <main id='Questions-and-controls-container'>
                 <section className='quiz-question'>
                     <form className='question-group'>
-                        <p>{quizQuestions[0]}</p>
+                        <p>{props.quizQuestions[0]}</p>
                         <div className='answer-choices'>
                             {questionOneAnswerChoices}
                         </div>
                     </form>
                     <hr/>
                 </section>
-                <button className='CTA-button'>Check answers</button>
+                <button className='CTA-button' onClick={props.handleCheckClick}>Check answers</button>
             </main>
             <img id="Quest-blue-blob" src={QuestBlueBlob} alt='decorative image'/>
         </>
