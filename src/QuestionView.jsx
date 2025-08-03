4561b91ee6cdd62fbb8c1c39213ea268171af5ec
Stephon Odom <stephon.odom7@gmail.com>
2025-08-03 16:16:27 -0400

    Remove commented out code and notes.

--- src/QuestionView.jsx
+++ src/QuestionView.jsx
@@ -26,122 +26,6 @@ export default function QuestionsView(props){
             )
         ) 
     })
-    
-    // const questionOneAnswerChoices = props.quizRandomizedAnswers[0].map((questionCollection, index) => 
-    //     {   
-    //         return(
-    //             <div key={nanoid()} className='answer-choice-container'>
-    //                 <input key={nanoid()} 
-    //                     type="radio" 
-    //                     name="question-one"
-    //                     id={`question-one-button: ${index}`} 
-    //                     value={questionCollection} 
-    //                     checked={props[`selectedAnswer${0}`] === `${questionCollection}`} 
-    //                     onChange={props[`handleAnswerChoiceClick${0}`]}
-    //                 />
-    //                 <label key={nanoid()} htmlFor={`question-one-button: ${index}`}
-    //                     >{questionCollection}
-    //                 </label>
-    //             </div>
-    //         )
-    //     }
-    // )
-
-    // const questionTwoAnswerChoices = props.quizRandomizedAnswers[1].map((questionCollection, index) => 
-    //     {   
-    //         // const isCorrect = quizAnswerKey.includes(questionCollection)
-    //         // className={clsx(
-    //         //     ['correct', isCorrect && props.isAnswersChecked ]
-    //         // )}
-    //         return(
-    //             <div key={nanoid()} className='answer-choice-container'>
-    //                 <input key={nanoid()} 
-    //                     type="radio"
-    //                     name="question-two"
-    //                     id={`question-two-button: ${index}`} 
-    //                     value={questionCollection} 
-    //                     checked={props.selectedAnswer2 === `${questionCollection}`} 
-    //                     onChange={props.handleAnswerChoiceClick2}
-    //                 />
-    //                 <label key={nanoid()} htmlFor={`question-two-button: ${index}`}
-    //                     >{questionCollection}
-    //                 </label>
-    //             </div>
-    //         )
-    //     }
-    // )
-
-    // const questionThreeAnswerChoices = props.quizRandomizedAnswers[2].map((questionCollection, index) => 
-    //     {   
-    //         // const isCorrect = quizAnswerKey.includes(questionCollection)
-    //         // className={clsx(
-    //         //     ['correct', isCorrect && props.isAnswersChecked ]
-    //         // )}
-    //         return(
-    //             <div key={nanoid()} className='answer-choice-container'>
-    //                 <input key={nanoid()} 
-    //                     type="radio"
-    //                     name="question-three"
-    //                     id={`question-three-button: ${index}`} 
-    //                     value={questionCollection} 
-    //                     checked={props.selectedAnswer3 === `${questionCollection}`} 
-    //                     onChange={props.handleAnswerChoiceClick3}
-    //                 />
-    //                 <label key={nanoid()} htmlFor={`question-three-button: ${index}`}
-    //                     >{questionCollection}
-    //                 </label>
-    //             </div>
-    //         )
-    //     }
-    // )
-
-    // const questionFourAnswerChoices = props.quizRandomizedAnswers[3].map((questionCollection, index) => 
-    //     {   
-    //         // const isCorrect = quizAnswerKey.includes(questionCollection)
-    //         // className={clsx(
-    //         //     ['correct', isCorrect && props.isAnswersChecked ]
-    //         // )}
-    //         return(
-    //             <div key={nanoid()} className='answer-choice-container'>
-    //                 <input key={nanoid()} 
-    //                     type="radio"
-    //                     name="question-four"
-    //                     id={`question-four-button: ${index}`} 
-    //                     value={questionCollection} 
-    //                     checked={props.selectedAnswer4 === `${questionCollection}`} 
-    //                     onChange={props.handleAnswerChoiceClick4}
-    //                 />
-    //                 <label key={nanoid()} htmlFor={`question-four-button: ${index}`}
-    //                     >{questionCollection}
-    //                 </label>
-    //             </div>
-    //         )
-    //     }
-    // )
-
-    // const questionFiveAnswerChoices = props.quizRandomizedAnswers[4].map((questionCollection, index) => 
-    //     {   
-    //         // const isCorrect = quizAnswerKey.includes(questionCollection)
-    //         // className={clsx(
-    //         //     ['correct', isCorrect && props.isAnswersChecked ]
-    //         // )}
-    //         return(
-    //             <div key={nanoid()} className='answer-choice-container'>
-    //                 <input key={nanoid()} 
-    //                     type="radio"
-    //                     name="question-five"
-    //                     id={`question-five-button: ${index}`} 
-    //                     value={questionCollection} 
-    //                     checked={props.selectedAnswer5 === `${questionCollection}`} 
-    //                     onChange={props.handleAnswerChoiceClick5}
-    //                 />
-    //                 <label key={nanoid()} htmlFor={`question-five-button: ${index}`}
-    //                     >{questionCollection}
-    //                 </label>
-    //             </div>
-    //         )
-    //     }
-    // )
 
     return(
         <>
@@ -153,35 +37,30 @@ export default function QuestionsView(props){
                             <legend>{props.quizQuestions[0]}</legend>
                             <div className='answer-choices'>
                                 {quizAnswerCollections[0]}
-                                {/* {questionOneAnswerChoices} */}
                             </div>
                         </fieldset>
                         <fieldset>
                             <legend>{props.quizQuestions[1]}</legend>
                             <div className='answer-choices'>
                                 {quizAnswerCollections[1]}
-                                {/* {questionTwoAnswerChoices} */}
                             </div>
                         </fieldset>
                         <fieldset>
                             <legend>{props.quizQuestions[2]}</legend>
                             <div className='answer-choices'>
                                 {quizAnswerCollections[2]}
-                                {/* {questionThreeAnswerChoices} */}
                             </div>
                         </fieldset>
                         <fieldset>
                             <legend>{props.quizQuestions[3]}</legend>
                             <div className='answer-choices'>
                                 {quizAnswerCollections[3]}
-                                {/* {questionFourAnswerChoices} */}
                             </div>
                         </fieldset>
                         <fieldset>
                             <legend>{props.quizQuestions[4]}</legend>
                             <div className='answer-choices'>
                                 {quizAnswerCollections[4]}
-                                {/* {questionFiveAnswerChoices} */}
                             </div>
                         </fieldset>
                     </form>
