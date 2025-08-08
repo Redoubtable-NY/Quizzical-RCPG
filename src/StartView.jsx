7d2067bdf4ca421d600e011d29aad768a088bbf2
Stephon Odom <stephon.odom7@gmail.com>
2025-08-08 16:58:25 -0400

    Add new section JSX element to house the JSX elements within the start view so they can be more effectively styled as a group for layout purposes.

--- src/StartView.jsx
+++ src/StartView.jsx
@@ -4,14 +4,16 @@ import blueBlob from '../public/Start_view/Blue_blob.svg'
 export default function StartView(props){
     return(
         <>
-            <img id="start-yellow-blob" src={yellowBlob} alt='decorative image'/>
-            <header>
-                <h1 id="site-header">Quizzical</h1>
-                <p id="site-header-description">Quick trivia to exercise your mind.</p>
-                <button onClick={props.handleClick} className='CTA-button'>Start quiz</button>
-                {props.fetchError ? <p id="trivia-error">There's been an error please wait 10 seconds and refresh the page.</p> : null}
-            </header>
-            <img id="start-blue-blob" src={blueBlob} alt='decorative image'/>
+            <section id='start-view-container'>
+                <img id="start-yellow-blob" src={yellowBlob} alt='decorative image'/>
+                <header>
+                    <h1 id="site-header">Quizzical</h1>
+                    <p id="site-header-description">Quick trivia to exercise your mind.</p>
+                    <button onClick={props.handleClick} className='CTA-button'>Start quiz</button>
+                    {props.fetchError ? <p id="trivia-error">There's been an error please wait 10 seconds and refresh the page.</p> : null}
+                </header>
+                <img id="start-blue-blob" src={blueBlob} alt='decorative image'/>
+            </section>
         </>
     )
 }
